e9f7b8023a1e74dead147697b994fdc6502e5609
rohan1827 <rohan.aryan2001@gmail.com>
2024-11-01 21:46:16 -0400

    changed streamlit app, added few functionalities

--- demo-app/streamlitapp.py
+++ demo-app/streamlitapp.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import os
 import imageio
+from moviepy.editor import VideoFileClip
 
 import tensorflow as tf
 from utils import load_data, num_to_char
@@ -16,47 +17,56 @@ with st.sidebar:
     st.title("Lip Sense :) ")
     st.info("Inspired by the LipNet Model!.")
 
-st.title("LipNet App")
+# st.title("LipNet App")
+st.markdown(
+    "<h1 style='text-align: center; color: vividblue;'>LipNet App!</h1>",
+    unsafe_allow_html=True,
+)
 # Generating a list of options or videos
-options = os.listdir(r"D:\repositories\Lip-Sense\demo-data\s1")
+options = os.listdir(
+    os.path.join("..", "demo-data", "videos", "s1")
+)  # change the directory to bring the S1 out of the videos and just have it there.
 
 selected_video = st.selectbox("Choose video", options)
 
-file_path = os.path.join(r"D:\repositories\Lip-Sense\demo-data\s1", selected_video)
 
-# # Generate two columns
-# col1, col2 = st.columns(2)
+# Generate two columns
+col1, col2 = st.columns(2)
 
-# if options:
+if options:
 
-#     # Rendering the video
-#     with col1:
-#         pass
-#         st.info("The video below displays the converted video in mp4 format")
-#         # file_path = os.path.join("..", "data", "s1", selected_video)
-#         file_path = f"D:\repositories\Lip-Sense\demo-data\s1\{selected_video}"
-#         os.system(f"ffmpeg -i {file_path} -vcodec libx264 test_video.mp4 -y")
+    # Rendering the video
+    with col1:
+        st.text("This is my col 1")
+        file_path = os.path.join("..", "demo-data", "videos", "s1", selected_video)
 
-#         # Rendering inside of the app
-#         video = open("test_video.mp4", "rb")
-#         video_bytes = video.read()
-#         st.video(video_bytes)
+        # Convert video to mp4 format ; Load the .mpg video
+        clip = VideoFileClip(file_path)
+        # Write the video as .mp4
+        output_path = os.path.join("..", "demo-data", "test video.mp4")
+        clip.write_videofile(output_path, codec="libx264")
 
-#     with col2:
-#         st.info("This is all the machine learning model sees when making a prediction")
-#         video, annotations = load_data(tf.convert_to_tensor(file_path))
-#         imageio.mimsave("animation.gif", video, fps=10)
-#         st.image("animation.gif", width=400)
+        # # # Rendering inside of the app
+        # video = open("test_video.mp4", "rb")
+        # video_bytes = video.read()
+        # st.video(video_bytes)
 
-#         st.info("This is the output of the machine learning model as tokens")
-#         model = load_model()
-#         yhat = model.predict(tf.expand_dims(video, axis=0))
-#         decoder = tf.keras.backend.ctc_decode(yhat, [75], greedy=True)[0][0].numpy()
-#         st.text(decoder)
+    with col2:
+        st.text("This is my col 2")
+        # st.info("This is all the machine learning model sees when making a prediction")
+        # video, annotations = load_data(tf.convert_to_tensor(file_path))
+        # imageio.mimsave("animation.gif", video, fps=10)
+        # st.image("animation.gif", width=400)
 
-#         # Convert prediction to text
-#         st.info("Decode the raw tokens into words")
-#         converted_prediction = (
-#             tf.strings.reduce_join(num_to_char(decoder)).numpy().decode("utf-8")
-#         )
-#         st.text(converted_prediction)
+        # st.info("This is the output of the machine learning model as tokens")
+        # model = load_model()
+        # yhat = model.predict(tf.expand_dims(video, axis=0))
+        # decoder = tf.keras.backend.ctc_decode(yhat, [75], greedy=True)[0][0].numpy()
+        # st.text(decoder)
+
+        # # Convert prediction to text
+        # st.info("Decode the raw tokens into words")
+        # converted_prediction = (
+        #     tf.strings.reduce_join(num_to_char(decoder)).numpy().decode("utf-8")
+        # )
+        # st.text(converted_prediction)
